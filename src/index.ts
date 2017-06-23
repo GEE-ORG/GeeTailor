b7af9b98fb632717e537a8aa45e268bbb591bb32
Geeku <ggongpei@qq.com>
2017-06-23 16:05:38 +0800

    seperate btns and add avatar rect

--- src/index.ts
+++ src/index.ts
@@ -15,28 +15,17 @@ const defaultOption: gtOption = {
 const template = `
 <div class="gt-canvas" style="display: inline-block">
     <canvas class="gt-img" style="background-color: rgba(0,0,0,.8)"></canvas>
+    <input class="gt-upload" type="file" accept="image/jpg,image/png" style="display: none">
+    <button class="gt-ctrl_upload">Upload</button>
     <div class="gt-info">
         <span class="gt-info_size"></span>
         <span class="gt-info_color"></span>
     </div>
 </div>
-<div class="gt-preview">
-    <img class="gt-preview_img" style="display: none" download="gt-img">
-</div>
-<section class="gt-ctrl">
-    <button class="gt-ctrl_upload">Upload</button>
-    <button class="gt-ctrl_export">Export</button>
-
-    <input class="gt-upload" type="file" accept="image/jpg,image/png" style="display: none">
-</section>
 `;
 const circleMask = `
 <svg width="100px" height="100px" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <path d="M0,0 L100,0 L100,100 L0,100 L0,0 Z M50,100 C77.6142375,100 100,77.6142375 100,50 C100,22.3857625 77.6142375,0 50,0 C22.3857625,0 0,22.3857625 0,50 C0,77.6142375 22.3857625,100 50,100 Z" id="Combined-Shape" stroke="none" fill-opacity="0.5" fill="#000000" fill-rule="evenodd"></path>
-</svg>`;
-const rectMask = `
-<svg width="100px" height="100px" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <path d="M0,0 L100,0 L100,100 L0,100 L0,0 Z M10,10 L90,10 L90,90 L10,90 L10,10 Z" id="Combined-Shape" stroke="none" fill-opacity="0.5" fill="#000000" fill-rule="evenodd"></path>
 </svg>`;
 
 export default class GeeTailor {
@@ -60,10 +49,11 @@ export default class GeeTailor {
 
     public upload: HTMLButtonElement;
     public export: HTMLButtonElement;
-    public preview: HTMLImageElement;
+
+    private rectMaskWidth = 0;
 
     private isMoving = false;
-    private previewImgUrl: string;
+    private hasChanged = true;
 
     private img: HTMLImageElement = new Image;
     private original = {
@@ -92,10 +82,8 @@ export default class GeeTailor {
         this.info['size'] = this.el.querySelector('.gt-info_size') as HTMLElement;
         this.info['color'] = this.el.querySelector('.gt-info_color') as HTMLElement;
         this.canvas = this.el.querySelector('.gt-img') as HTMLCanvasElement;
-        this.preview = this.el.querySelector('.gt-preview_img') as HTMLImageElement;
         this.upload = this.el.querySelector('.gt-upload') as HTMLButtonElement;
         this.btns['upload'] = this.el.querySelector('.gt-ctrl_upload') as HTMLButtonElement;
-        this.btns['export'] = this.el.querySelector('.gt-ctrl_export') as HTMLButtonElement;
 
         this.ctx = this.canvas.getContext('2d');
         // this.ctx.scale(this.dpr, this.dpr);
@@ -103,15 +91,19 @@ export default class GeeTailor {
 
         this.mode = this.option.mode;
         this.canvasInit();
-        this.previewInit();
 
         if (this.mode === 'avatar') {
-            const svgData = this.option.maskType === 'circle' ? circleMask : rectMask;
-            const maskData = toBlob(svgData, 'image/svg+xml');
-            this.svgMask.onload = () => {
-                this.ctx.drawImage(this.svgMask, 0, 0, this.canvas.width, this.canvas.width);
-            };
-            this.svgMask.src = toUrl(maskData);
+            if (this.option.maskType === 'circle') {
+                const maskData = toBlob(circleMask, 'image/svg+xml');
+                this.svgMask.onload = () => {
+                    this.ctx.drawImage(this.svgMask, 0, 0, this.canvas.width, this.canvas.width);
+                };
+                this.svgMask.src = toUrl(maskData);
+            } else if (this.option.maskType === 'rect') {
+                this.rectMaskWidth = this.canvas.width / 10;
+                this.canvas.width += this.rectMaskWidth * 2;
+                this.canvas.height += this.rectMaskWidth * 2;
+            }
         }
 
         this.addEvents();
@@ -137,7 +129,6 @@ export default class GeeTailor {
         this.drawBg();
         this.drawImg();
         this.drawMask();
-        this.setPreview();
     }
 
     private drawBg () {
@@ -154,43 +145,24 @@ export default class GeeTailor {
     }
 
     private drawMask () {
-        this.ctx.drawImage(this.svgMask, 0, 0, this.canvas.width, this.canvas.width);
-    }
-
-    private previewInit () {
-
-        let cssText = `
-            display: inline-block;
-            width: ${this.width}px;
-            height: ${this.height}px;
-            background: rgba(0,0,0,.8);
-            background-size: contain;
-        `;
-
-        if (
-            this.mode === 'avatar' &&
-            this.option.maskType === 'circle'
-        ) {
-            cssText += 'border-radius: 50%';
+        if (this.mode === 'avatar') {
+            const maskType = this.option.maskType;
+            if (maskType === 'circle') {
+                this.ctx.drawImage(this.svgMask, 0, 0, this.canvas.width, this.canvas.width);
+            } else if (maskType === 'rect') {
+                const canvasWidth = this.canvas.width;
+                this.ctx.fillStyle = 'rgba(0,0,0,.5)';
+                this.ctx.fillRect(0, 0, canvasWidth, this.rectMaskWidth,);
+                this.ctx.fillRect(0, canvasWidth - this.rectMaskWidth, canvasWidth, this.rectMaskWidth);
+                this.ctx.fillRect(0, this.rectMaskWidth, this.rectMaskWidth, canvasWidth - this.rectMaskWidth * 2);
+                this.ctx.fillRect(canvasWidth - this.rectMaskWidth, this.rectMaskWidth, this.rectMaskWidth, this.canvas.height - this.rectMaskWidth * 2);
+            }
         }
-
-        this.preview.parentElement.style.cssText = cssText;
-    }
-    public setPreview () {
-
-        // if (this.isMoving) return;
-
-        // this.preview.style.backgroundImage = `url(${this.canvas.toDataURL()})`
-
-        // this.output.toBlob(blob => this.preview.src = this.toUrl(blob));
     }
 
     private addEvents () {
         this.btns['upload'].addEventListener('click', () => {
             this.upload.click();
-        });
-        this.btns['export'].addEventListener('click', () => {
-            this.download();
         });
         this.upload.addEventListener('change', (e: any) => {
             
@@ -241,7 +213,6 @@ export default class GeeTailor {
             requestAnimationFrame(this.render.bind(this));
             st = setTimeout(() => {
                 isScaling = false;
-                this.setPreview();
             }, 200);
         });
 
@@ -270,7 +241,6 @@ export default class GeeTailor {
         document.addEventListener('mouseup', e => {
             isMoving = false;
             document.body.style.cursor = 'auto';
-            this.setPreview();
         })
     }
 
@@ -310,15 +280,15 @@ export default class GeeTailor {
         switch (val) {
             case 'avatar': {
                 this.option.width > this.option.height ?
-                    [this.width, this.height] = [this.option.height, this.option.height] :
-                    [this.width, this.height] = [this.option.width, this.option.width];
+                    this.width = this.height = this.option.height :
+                    this.width = this.height = this.option.width;
                 break;
             }
             case 'free': {
                 this.width = this.option.width;
                 this.height = this.option.height;
                 break;
-            }
+            } 
         }
     }
 
@@ -330,6 +300,7 @@ export default class GeeTailor {
         deltaWidth < 100 && (deltaWidth = 100); 
         this.imgBounding.width = deltaWidth;
         this.imgBounding.height = deltaWidth / this.ratio;
+        this.hasChanged = true;
     }
     get imgHeight () {
         return this.imgBounding.height;
@@ -339,6 +310,7 @@ export default class GeeTailor {
         deltaHeight < 100 && (deltaHeight = 100); 
         this.imgBounding.height = deltaHeight;
         this.imgBounding.width = deltaHeight * this.ratio;
+        this.hasChanged = true;
     }
     get imgOffsetX () {
         return this.imgBounding.offsetX;
@@ -349,6 +321,7 @@ export default class GeeTailor {
         val = val > rightLine ? rightLine : (val < -this.imgWidth ? leftLine : val);
 
         this.imgBounding.offsetX = val;
+        this.hasChanged = true;
     }
     get imgOffsetY () {
         return this.imgBounding.offsetY;
@@ -359,54 +332,63 @@ export default class GeeTailor {
         val = val > this.canvas.height ? topLine : (val < -this.imgHeight ? bottomLine: val);
 
         this.imgBounding.offsetY = val;
-    }
-    private _scale: number = 0;
-    get scale () {
-        return this._scale;
-    }
-    set scale (val) {
-        this._scale = val;
-        this.render()    
+        this.hasChanged = true;
     }
 
     private createImg () {
-        const width = this.canvas.width / this.dpr;
-        const height = this.canvas.height / this.dpr;
+
+        if (!this.hasChanged) return;
+
+        const width = this.width;
+        const height = this.width;
         this.output.width = width;
         this.output.height = height;
         this.outCtx.beginPath();
 
-        if (
-            this.mode === 'avatar' && 
-            this.option.maskType === 'circle'
-        ) {
-            this.outCtx.arc(
-                width / 2,
-                height / 2,
-                width / 2,
-                0,
-                2 * Math.PI,
-                true
-            );
+        let clipArea: [number, number, number, number];
+
+        if (this.mode === 'avatar') {
+            if (this.option.maskType === 'circle') {
+                this.outCtx.arc(
+                    width / 2,
+                    height / 2,
+                    width / 2,
+                    0,
+                    2 * Math.PI,
+                    true
+                );
+                this.outCtx.clip();
+                clipArea = [0, 0, width, height];
+            } else if (this.option.maskType === 'rect') {
+                clipArea = [
+                    this.rectMaskWidth, 
+                    this.rectMaskWidth, 
+                    this.canvas.width - this.rectMaskWidth * 2, 
+                    this.canvas.width - this.rectMaskWidth * 2,
+                    0,
+                    0,
+                    width, 
+                    height,
+                ];
+                // const imgAreaWidth = (this.canvas.width - this.rectMaskWidth * 2) / this.dpr;
+                // this.outCtx.rect(0, 0, width, width);
+            }
         } 
-    
-        this.outCtx.clip();
-        this.outCtx.drawImage(
-            this.canvas, 
-            0, 
-            0, 
-            width, 
-            height
-        );
+        
+        (this.outCtx.drawImage as any)(this.canvas, ...clipArea);
+
+        this.hasChanged = false;
     }
 
     public toBase64 () {
         this.createImg();
         return this.output.toDataURL();
     }
-    public toBlob () {
+    public toBlob (cb) {
         this.createImg();
-        return this.output.toDataURL();
+        return new Promise((r, j) => {
+            this.output.toBlob(blob => r(blob));
+        });
     }
     public download () {
         this.createImg();
